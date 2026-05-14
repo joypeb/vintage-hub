c93a6f0a563a8759e2b70da143193cacfd734612
joypeb <joypeb@naver.com>
2026-05-14 17:59:09 +0900

    [FEAT] first commit

---
+++ src/main/java/com/joypeb/vintagehub/VintageHubBeApplication.java
@@ -0,0 +1,13 @@
+package com.joypeb.vintagehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintageHubBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VintageHubBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joypeb/vintagehub/VintageHubBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joypeb.vintagehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VintageHubBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
